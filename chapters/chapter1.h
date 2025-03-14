fce87f464240cda49a7f1d45efd03fedf4e25509
t <87922964+PhotonMastr@users.noreply.github.com>
2025-03-14 15:21:32 -0400

    Create chapter1.h

---
+++ chapters/chapter1.h
@@ -0,0 +1,15 @@
+#ifndef CHAPTER1_H
+#define CHAPTER1_H
+
+#include <iostream>
+
+// Function declarations
+void emergeFromBunkerScenario();
+void showStartOfChapter1Menu(int &choice);
+void emergeFromBunkerHandling();
+void investigateNearbyRuins();
+void followTheRoad();
+void postApocalypse();
+void goRight();
+void headingToRuinedCity();
+#endif // CHAPTER1_H
